99ac2187b2593c371ca3040d5bdfda9b9e8fb959
MateusEneas <mateus.eneas98@gmail.com>
2023-03-09 10:10:38 -0300

    primeiro teste

---
+++ dio/me/br/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package dio.me.br;
+
+import dio.me.br.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+       /* int a = 5;
+        int b = 2;
+
+        System.out.println("Olá, mundo!");*/
+
+    }
+}
